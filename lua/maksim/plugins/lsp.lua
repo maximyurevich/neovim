2d4f656d2801c10758562948759f4c8a053a5d93
Maksim Yurevich <jam-april-ocean@duck.com>
2025-06-28 02:12:44 +0300

    feat(lazydev): load luvit types

--- lua/maksim/plugins/lsp.lua
+++ lua/maksim/plugins/lsp.lua
@@ -27,6 +27,11 @@ return {
       {
         "folke/lazydev.nvim",
         ft = "lua",
+        opts = {
+          library = {
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
       },
       {
         "onsails/lspkind.nvim",
