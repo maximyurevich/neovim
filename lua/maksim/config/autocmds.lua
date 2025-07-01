720568ca40331a2ee5bac608719a2e99d508e546
Maxim Yurevich <maxim.yurewitch@gmail.com>
2025-07-01 04:37:46 +0300

    feat(autocmd): disable continuation of comments to the next line

--- lua/maksim/config/autocmds.lua
+++ lua/maksim/config/autocmds.lua
@@ -73,3 +73,14 @@ autocmd("LspAttach", {
     end, opts)
   end,
 })
+
+-- Comments
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "r", "o" })
+  end,
+})
+
+-- Filetypes
